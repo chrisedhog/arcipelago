569cc6ca262e5ef5d6d1bf9746a63e2ba3d86872
Chris Hogben <chris.edhogben@gmail.com>
2016-03-19 13:44:06 +1100

    forgot what I did

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,13 @@ class UsersController < ApplicationController
     def make_admin
         @user = User.find(params[:id])
         @user.add_role :admin
-        redirect_to :back, alert: "Success!"
+        redirect_to :back, alert: 'User is now an admin'
     end
     
     def remove_admin
         @user = User.find(params[:id])
         @user.remove_role :admin
-        redirect_to :back, alert: "Success!"
+        redirect_to :back, alert: 'Admin privileges removed'
     end
     
     def destroy
