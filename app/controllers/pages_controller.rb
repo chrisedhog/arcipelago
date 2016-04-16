f502ed0f52973573e66f7531406169fa3671103d
Chris Hogben <chris.edhogben@gmail.com>
2016-04-16 15:54:48 +1000

    trying to get charts working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
       if current_user.has_role? :admin
           @users = User.all
           @group_categories = GroupCategory.all
-          @line_graph = User.group_by_day(:created_at, format: "%a %d%b", range: 1.year.ago.midnight..Time.now, time_zone: "Sydney")
+#          @line_graph = User.group_by_day(:created_at, format: "%a %d%b", range: 1.year.ago.midnight..Time.now, time_zone: "Sydney")
           
       else
           redirect_to root_path
